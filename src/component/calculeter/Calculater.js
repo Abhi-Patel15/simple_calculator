78dd51b33a44d01170edf2d43c9b19f2f2c9450b
abhi hothi <abhi.hothi@codefencers.com>
2023-05-23 10:55:23 +0530

    first commit

---
+++ src/component/calculeter/Calculater.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import "./calculator.css";
+
+const Calculater = () => {
+  const [display, setDisplay] = useState("");
+  console.log("Display", display);
+
+  console.log("display", display);
+
+  const handleClick = (e) => {
+    setDisplay(display?.concat(e?.target?.name));
+  };
+
+  const Calculater = () => {
+    try {
+      setDisplay(eval(display).toString());
+    } catch {
+      setDisplay("Error");
+    }
+  };
+
+  const clear = () => {
+    setDisplay("");
+  };
+  const backpase = () => {
+    setDisplay(display.slice(0, -1));
+  };
+
+  return (
+    <div>
+      <div className="calculator">
+        <h1>Simple Calculator</h1>
+          <div className="result">
+            <input type="text" value={display} />
+        </div>
+   
+      <div className="button">
+        {/* <button   >%</button> */}
+        <button onClick={clear}>clear</button>
+
+        <button onClick={backpase}>C</button>
+        <br />
+
+        <button name="1" onClick={handleClick}>
+          1
+        </button>
+        <button name="2" onClick={handleClick}>
+          2
+        </button>
+        <button name="3" onClick={handleClick}>
+          3
+        </button>
+
+        <button name="0" onClick={handleClick}>
+          0
+        </button>
+
+        <button name="9" onClick={handleClick}>
+          9
+        </button>
+
+        <button name="4" onClick={handleClick}>
+          4
+        </button>
+        <button name="5" onClick={handleClick}>
+          5
+        </button>
+        <button name="6" onClick={handleClick}>
+          6
+        </button>
+        <button name="7" onClick={handleClick}>
+          7
+        </button>
+        <button name="8" onClick={handleClick}>
+          8
+        </button>
+
+        <button name="/" onClick={handleClick}>
+          ÷
+        </button>
+        <button name="*" onClick={handleClick}>
+          x
+        </button>
+        <button name="-" onClick={handleClick}>
+          -
+        </button>
+        <button name="+" onClick={handleClick}>
+          +
+        </button>
+        <button name="=" onClick={Calculater}>
+          =
+        </button>
+        <br />
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Calculater;
